6b487eaf609ccf0bc41fa0d262da6c4623af80b6
ltzz <ltzz@users.noreply.github.com>
2020-10-11 01:16:53 +0900

    レイアウトをXMLから読めるように
    Signed-off-by: ltzz <ltzz@users.noreply.github.com>

--- README.md
+++ README.md
@@ -1,1 +1,51 @@
-# terra
+# 開発ガイド
+## Windows
+
+### MSYS2を入れる
+
+### インストール先のパスを通す
+
+C:\msys64\mingw64\bin
+
+他のgccとか入れてる場合は、より上位にしておく
+
+### MSYSで
+```
+pacman -S mingw-w64-x86_64-toolchain
+pacman -S mingw-w64-x86_64-gtk3
+pacman -S mingw-w64-x86_64-glade #必要に応じて
+pacman -S mingw-w64-x86_64-pkg-config
+```
+しておく
+
+```
+$ pacman -Sl | grep gtk3
+mingw32 mingw-w64-i686-gtk3 3.24.22-1
+mingw64 mingw-w64-x86_64-gtk3 3.24.22-1
+```
+
+### 実行前に
+
+```
+rustup toolchain install stable-x86_64-pc-windows-gnu
+rustup default nightly-x86_64-pc-windows-gnu
+```
+しておく
+
+### 実行
+
+```
+cargo run
+```
+
+### 開発
+
+
+/mingw64/bin/glade で画面を作成
+
+
+# 参考リンク
+
+https://www.gtk.org/docs/installations/windows/#using-gtk-from-msys2-packages
+
+http://gtk-rs.org/docs/gtk/
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,20 @@
 extern crate gtk;
+
 use gtk::prelude::*;
-use gtk::{ButtonsType, DialogFlags, MessageDialog, MessageType, Window};
 
 fn main() {
-    if gtk::init().is_err() {
-        println!("Failed to initialize GTK.");
-        return;
-    }
-    MessageDialog::new(
-        None::<&Window>,
-        DialogFlags::empty(),
-        MessageType::Info,
-        ButtonsType::Ok,
-        "Hello World",
-    )
-    .run();
+    gtk::init().expect("Failed to initialize GTK.");
+
+    let ui = include_str!("layout/main.ui");
+    let builder = gtk::Builder::new_from_string(ui);
+    
+    let window1 : gtk::Window = builder.get_object("main_window").unwrap();
+    window1.connect_delete_event(move |_, _| {
+        gtk::main_quit();
+        Inhibit(false)
+    });
+
+    window1.show_all();
+    
+    gtk::main();
 }
