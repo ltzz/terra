472da46f79d4853c5cbbceb0f36b120bd3c40ff2
ltzz <ltzz@users.noreply.github.com>
2020-10-13 22:01:23 +0900

    refactor: 設計作り込みのためのリファクタ
    Signed-off-by: ltzz <ltzz@users.noreply.github.com>

--- src/app.rs
+++ src/app.rs
@@ -2,76 +2,68 @@ extern crate gtk;
 
 use std::{cell::RefCell, rc::Rc};
 
-use gtk::prelude::*;
+use crate::initialize::initialize::Setting;
+
 use super::{initialize::*, timeline::htl::get_toots, util::html_parse};
+use gtk::prelude::*;
 
 pub struct Data {
-  pub articles: Vec<Article>
+    pub articles: Vec<Article>,
 }
 
 pub struct Article {
-  pub username: String,
-  pub description: String
+    pub username: String,
+    pub description: String,
 }
 
+pub struct App {
+    articles: Rc<RefCell<Vec<Article>>>,
+    setting: Setting
+}
 
-pub struct App{
-    pub articles : Rc<RefCell<Vec<Article>>>
-  }
-  
 impl App {
-  pub fn new() -> Self {
-  
-    // glib::set_program_name(APP_NAME.into());
+    pub fn new() -> Self {
+        // glib::set_program_name(APP_NAME.into());
 
-    gtk::init().expect("Failed to initialize GTK.");
-  
-    // let ui = Rc::new(RefCell::new(GtkUi::new(&auth, &content)));
-    let articles = Rc::new(RefCell::new(Vec::new()));
-    Self{articles}
-  }
-  
-  
-  pub fn start(self) -> Self {
+        gtk::init().expect("Failed to initialize GTK.");
 
-    gtk::init().expect("Failed to initialize GTK.");
+        // let ui = Rc::new(RefCell::new(GtkUi::new(&auth, &content)));
+        let articles = Rc::new(RefCell::new(Vec::new()));
 
-    let ui = include_str!("layout/main.ui");
-    let builder = gtk::Builder::new_from_string(ui);
-    
-    let window1 : gtk::Window = builder.get_object("main_window").unwrap();
-    window1.connect_delete_event(move |_, _| {
-        gtk::main_quit();
-        Inhibit(false)
-    });
+        let setting = initialize::initialize().ok().unwrap();
 
-    window1.show_all();
+        Self { articles, setting }
+    }
 
-    let setting = initialize::initialize();
+    pub fn start(self) -> Self {
+        let ui = include_str!("layout/main.ui");
+        let builder = gtk::Builder::new_from_string(ui);
 
-    let listbox: gtk::ListBox = builder.get_object("listbox").unwrap();
+        let window1: gtk::Window = builder.get_object("main_window").unwrap();
+        window1.connect_delete_event(move |_, _| {
+            gtk::main_quit();
+            Inhibit(false)
+        });
 
-    let toots = setting.map(|op| 
-        op.instance_settings.get(0)
-        .map(|is|
-            get_toots(is)
-        )
-    );
-    
-    
-    let articles: Vec<Article> = toots.unwrap().unwrap().unwrap().into_iter()
-    .map(|t|
-        Article{
-            username: t.account.display_name,
-            description: html_parse::html_to_text(t.content.as_ref())
-        }
-    ).collect();
-    
-    let mut data: Data = Data{articles: articles};
-    
-    let _ = data.articles.iter().for_each(
-        |article| {
-                
+        window1.show_all();
+
+        let listbox: gtk::ListBox = builder.get_object("listbox").unwrap();
+
+        let toots = self.setting.instance_settings.get(0).map(|is| get_toots(is));
+
+        let articles: Vec<Article> = toots
+            .unwrap()
+            .unwrap()
+            .into_iter()
+            .map(|t| Article {
+                username: t.account.display_name.clone(),
+                description: html_parse::html_to_text(t.content.as_ref()).clone(),
+            })
+            .collect();
+
+        let data: Data = Data { articles: articles };
+
+        let _ = data.articles.iter().for_each(|article| {
             // let display_name = gtk::Entry::new();
             // display_name.set_text(toot.account.display_name.as_ref());
 
@@ -85,22 +77,28 @@ impl App {
             clabel.set_halign(gtk::Align::Start);
             clabel.set_line_wrap(true);
 
-            let hbox : gtk::Box = gtk::Box::new(gtk::Orientation::Horizontal, 0);
+            let hbox: gtk::Box = gtk::Box::new(gtk::Orientation::Horizontal, 0);
             hbox.pack_start(&display_name, true, true, 0);
             hbox.pack_start(&clabel, true, true, 0);
 
             let r = gtk::ListBoxRow::new();
             r.add(&hbox);
             listbox.prepend(&r);
-            
-        }
-    );
-    
-    listbox.set_size_request(200, 200);
-    listbox.show_all();
+        });
 
-    gtk::main();
+        listbox.set_size_request(200, 200);
+        listbox.show_all();
 
-    self
-  }
+        let load_button: gtk::Button = builder.get_object("load_button").unwrap();
+
+        let articles = self.articles.clone();
+        load_button.connect_button_release_event(move |_, _| {
+            *articles.borrow_mut() = Vec::new();
+            gtk::Inhibit(false)
+        });
+
+        gtk::main();
+
+        self
+    }
 }
